766226b2fe4cdd51227cb5f341c3d9ab9d143b53
MINJUN17 <71377165+MINJUN17@users.noreply.github.com>
2026-02-27 18:00:22 +0900

    임시제출

--- Blackjack-1/BlackJack.cs
+++ Blackjack-1/BlackJack.cs
@@ -1,96 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Windows.Markup;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
-
-
-//class Card
-//{
-//    public string Shape { get; }
-//    public string Number { get; }
-//    public Card(string shape, string number)
-//    {
-//        Shape = shape;
-//        Number = number;
-//    }
-//}
-
-
 class CardDeck
 {
-    //public string Shape { get; }
-    //public string Number { get; }
-    //public CardDeck(string shape, string number)
-    //{
-    //    Shape = shape;
-    //    Number = number;
-    //}
+    private string[] Shapes = { "♥", "◆", "♣", "♠" };
+    private string[] Numbers = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+    private string[] Cards;
+    //private int handCount = 0;
+    Random number = new Random();
 
-    
-        private string[] Shapes = { "♥", "◆", "♣", "♠" };
-        private string[] Numbers = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
-        //private Card[] _cards;
-        private string[] _cards;
-
-
-
-    //private string[,] Cards = new string[4, 13]
-    //{
-    //    { "♥ A","♥ 2","♥ 3","♥ 4","♥ 5","♥ 6","♥ 7","♥ 8","♥ 9","♥ 10","♥ J","♥ Q","♥ K"},
-    //    { "◆ A","◆ 2","◆ 3","◆ 4","◆ 5","◆ 6","◆ 7","◆ 8","◆ 9","◆ 10","◆ J","◆ Q","◆ K"},
-    //    { "♣ A","♣ 2","♣ 3","♣ 4","♣ 5","♣ 6","♣ 7","♣ 8","♣ 9","♣ 10","♣ J","♣ Q","♣ K"},
-    //    { "♠ A","♠ 2","♠ 3","♠ 4","♠ 5","♠ 6","♠ 7","♠ 8","♠ 9","♠ 10","♠ J","♠ Q","♠ K"}
-    //};
-
-
-    public string RandomCard()
-    {
-        Random number = new Random();
-        int numbers = number.Next(52);
-        return _cards[numbers];
-    }
-
-    public void Card()
+    public CardDeck()
     {
         int k = 0;
-        _cards = new string[Shapes.Length * Numbers.Length];
-        //foreach(string shape in Shapes)
-        //{
-        //    foreach(string number in Numbers)
-        //    {
-        //        _cards[i++] = new Card(shape, number);
-        //    }
-        //}
-      
-        for(int i = 0; i < Shapes.Length; i++)
+        Cards = new string[Shapes.Length * Numbers.Length];
+
+        for (int i = 0; i < Shapes.Length; i++)
         {
-            for(int j = 0; j < Numbers.Length; j++)
+            for (int j = 0; j < Numbers.Length; j++)
             {
-                _cards[k++] = $"{Shapes[i]}{Numbers[j]}";
+                Cards[k++] = $"{Shapes[i]}{Numbers[j]}";
             }
         }
     }
-    
 
+    public string RandomCard()
+    {
+        int numbers = number.Next(52);
+        return Cards[numbers];
+    }
 
-    //public string PickRandomCell(string[,] board, Random rng)
+    //public void Shuffle()
     //{
-    //    // 행 개수, 열 개수
-    //    int rows = board.GetLength(0);
-    //    int cols = board.GetLength(1);
 
-    //    // 0 ~ rows-1, 0 ~ cols-1 랜덤 좌표
-    //    int r = rng.Next(rows);
-    //    int c = rng.Next(cols);
+    //    for (int i = 0; i < Cards.Length; i++)
+    //    {
+    //        int randomIndex = number.Next(52);
 
-    //    // 그 위치의 문자열 반환
-    //    return board[r, c];
+    //        string temp = Cards[i];
+    //        Cards[i] = Cards[randomIndex];
+    //        Cards[randomIndex] = temp;
+    //    }
     //}
+    public static void AddCard(CardDeck deck, string[] hand, ref int handCount)
+    {
+        string card = deck.RandomCard();
 
+        // 이미 가진 카드면 다시 뽑기
+        for (int i = 0; i < handCount; i++)
+        {
+            if (hand[i] == card)
+            {
+                card = deck.RandomCard();
+                i = -1; // 처음부터 다시 검사
+            }
+        }
 
-
+        hand[handCount++] = card; // ✅ 여기서 1장 추가
+    }
+    public int GetNumValue(string card, int currentsum)
+    {
+        string numbercard = card.Substring(1);
+        if(numbercard == "A"&& currentsum > 11)
+        {
+            return 1;
+        }
+        else if(numbercard == "A" && currentsum <= 11)
+        {
+            return 11;
+        }
+        if (numbercard == "J" || numbercard == "Q" || numbercard == "K")
+        {
+            return 10;
+        }
+        return int.Parse(numbercard);
+    }
 }
 
