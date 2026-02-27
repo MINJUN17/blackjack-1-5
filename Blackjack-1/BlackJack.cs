6d2dfd29ce21e3937177f5e4a0607cb4617a1d56
MINJUN17 <71377165+MINJUN17@users.noreply.github.com>
2026-02-27 16:02:45 +0900

    1

--- Blackjack-1/BlackJack.cs
+++ Blackjack-1/BlackJack.cs
@@ -3,19 +3,20 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
 using System.Windows.Markup;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 
 
-class Card
-{
-    public string Shape { get; }
-    public string Number { get; }
-    public Card(string shape, string number)
-    {
-        Shape = shape;
-        Number = number;
-    }
-}
+//class Card
+//{
+//    public string Shape { get; }
+//    public string Number { get; }
+//    public Card(string shape, string number)
+//    {
+//        Shape = shape;
+//        Number = number;
+//    }
+//}
 
 
 class CardDeck
@@ -28,33 +29,53 @@ class CardDeck
     //    Number = number;
     //}
 
+    
+        private string[] Shapes = { "♥", "◆", "♣", "♠" };
+        private string[] Numbers = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+        //private Card[] _cards;
+        private string[] _cards;
 
-    //private string[] Shapes = { "♥", "◆", "♣", "♠" };
-    //private string[] Numbers = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
 
-    private string[,] Cards = new string[4, 13]
+
+    //private string[,] Cards = new string[4, 13]
+    //{
+    //    { "♥ A","♥ 2","♥ 3","♥ 4","♥ 5","♥ 6","♥ 7","♥ 8","♥ 9","♥ 10","♥ J","♥ Q","♥ K"},
+    //    { "◆ A","◆ 2","◆ 3","◆ 4","◆ 5","◆ 6","◆ 7","◆ 8","◆ 9","◆ 10","◆ J","◆ Q","◆ K"},
+    //    { "♣ A","♣ 2","♣ 3","♣ 4","♣ 5","♣ 6","♣ 7","♣ 8","♣ 9","♣ 10","♣ J","♣ Q","♣ K"},
+    //    { "♠ A","♠ 2","♠ 3","♠ 4","♠ 5","♠ 6","♠ 7","♠ 8","♠ 9","♠ 10","♠ J","♠ Q","♠ K"}
+    //};
+
+
+    public string RandomCard()
     {
-        { "♥ A","♥ 2","♥ 3","♥ 4","♥ 5","♥ 6","♥ 7","♥ 8","♥ 9","♥ 10","♥ J","♥ Q","♥ K"},
-        { "◆ A","◆ 2","◆ 3","◆ 4","◆ 5","◆ 6","◆ 7","◆ 8","◆ 9","◆ 10","◆ J","◆ Q","◆ K"},
-        { "♣ A","♣ 2","♣ 3","♣ 4","♣ 5","♣ 6","♣ 7","♣ 8","♣ 9","♣ 10","♣ J","♣ Q","♣ K"},
-        { "♠ A","♠ 2","♠ 3","♠ 4","♠ 5","♠ 6","♠ 7","♠ 8","♠ 9","♠ 10","♠ J","♠ Q","♠ K"}
-    };
-
-
-
-    public void RandomCard()
-    {
-        Random shape = new Random();
         Random number = new Random();
-        int shapes = shape.Next(4);
-        int numbers = number.Next(13);
+        int numbers = number.Next(52);
+        return _cards[numbers];
+    }
 
-    }
-    public void CardPrint(string shape, string number)
+    public void Card()
     {
-        int Shapes = this.shape;
-        Console.WriteLine(shape);
+        int k = 0;
+        _cards = new string[Shapes.Length * Numbers.Length];
+        //foreach(string shape in Shapes)
+        //{
+        //    foreach(string number in Numbers)
+        //    {
+        //        _cards[i++] = new Card(shape, number);
+        //    }
+        //}
+      
+        for(int i = 0; i < Shapes.Length; i++)
+        {
+            for(int j = 0; j < Numbers.Length; j++)
+            {
+                _cards[k++] = $"{Shapes[i]}{Numbers[j]}";
+            }
+        }
     }
+    
+
+
     //public string PickRandomCell(string[,] board, Random rng)
     //{
     //    // 행 개수, 열 개수
--- Blackjack-1/Program.cs
+++ Blackjack-1/Program.cs
@@ -1,9 +1,5 @@
 using System;
 
 CardDeck card = new CardDeck();
-Random rng = new Random();
-
-CardDeck card1 = card;
-
-string picked = CardDeck.PickRandomCell(card1, rng);
-Console.WriteLine($"·£´ý ¼±ÅÃ: {picked}");
+card.Card();
+Console.WriteLine(card.RandomCard());
